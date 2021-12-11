20a7a7c36600623df7a2222bd3089bc6fb956d27
Achilles8055 <josejimmy8055@gmail.com>
2021-12-11 07:43:56 +0530

    Facebook-loginpage

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $("#signup-form").validate({
+        rules:{
+        fname:{
+            required:true,
+            minlength:4
+        },
+        sname:{
+             required:true,
+            minlength:4
+        }
+        }
+    })
+})
